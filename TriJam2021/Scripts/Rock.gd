506476846e6a277ddb8b5a0d9368bf1a73e89212
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-20 15:05:43 -0400

    Rocks have health now and are destructable

--- TriJam2021/Scripts/Rock.gd
+++ TriJam2021/Scripts/Rock.gd
@@ -3,11 +3,16 @@ extends StaticBody2D
 var col_layer
 export var damage_amt = 1
 
+var health = 3
+
 func _ready():
 	$AnimationPlayer.play("Fall")
 	pass # Replace with function body.
 
-
+func damage(amt):
+	health -= amt
+	if health <= 0:
+		queue_free()
 
 
 func _on_Area2D_body_entered(body):
